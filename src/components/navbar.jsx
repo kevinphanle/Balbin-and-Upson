4d09f32eeff013952fc9d1d766090e8e583da5b8
Kevin Le <kevinphanle11@gmail.com>
2020-02-18 22:48:16 -0800

    splash page

---
+++ src/components/navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Navbar() {
+
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <a href="#">Balbin & Upson</a>
+      </div>
+
+      <div className="right-nav">
+        <ul className="nav-links">
+          <li className="nav-link">
+            <a href="#" className="link">Home</a>
+          </li>
+          <li className="nav-link">
+            <a href="#" className="link">Services</a>
+          </li>
+          <li className="nav-link">
+            <a href="#" className="link">Testimonials</a>
+          </li>
+          <li className="nav-link">
+            <a href="#" className="link">About Us</a>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar;
--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,10 +1,26 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+
+import Navbar from './navbar'
 
 function Portfolio() {
 
   return (
     <div className="wrapper">
-      Portfolio site
+      <Navbar />
+      
+      <div className="main">
+        <div className="intro">
+          <div className="intro-background"></div>
+
+          <div className="intro-group">
+            <div className="intro-logo">
+              <span className="logo-big-parts">Balbin</span> <span>&</span> <span className="logo-big-parts">Upson</span>
+            </div>
+            <p>Local Tax Business located in South San Jose, helping and educating families and local businesses on federal and state taxes.</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
