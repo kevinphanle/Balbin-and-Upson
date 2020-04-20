1673ef3eafcea0fddf0cfe70c916f2cab398bd14
Kevin Le <kevinphanle11@gmail.com>
2020-04-20 16:58:45 -0700

    cleaned up service card descriptions

--- src/components/frontpage/servicecardObjects.js
+++ src/components/frontpage/servicecardObjects.js
@@ -2,33 +2,33 @@
 let cards = [
   {
     "title": "Preparation",
-    "description": "preparing taxes for individuals, businesses, and rental properties",
+    "description": "Preparing taxes for individuals, businesses, and rental properties.",
     "icon": "address-card",
     "color": '#7FB069'
   },
   {
     "title": "Education",
-    "description": "givign knowledge to people who need it",
+    "description": "Giving knowledge to people who need it.",
     "icon": "lightbulb",
     "color": '#247f53'
   },
   {
     "title": "Planning",
-    "description": "planning the taxes long term",
+    "description": "Planning the taxes long term.",
     "icon": "money-bill-alt",
     "color": '#004346'
   },
   {
     "title": "Retirement",
-    "description": "planning the taxes for retirement",
+    "description": "Planning the taxes for retirement.",
     "icon": "map",
     "color": '#BCE784'
   },
   {
     "title": "Checkups",
-    "description": "checking up every quarter",
+    "description": "Checking up every quarter.",
     "icon": "calendar-check",
-    "color": 'dodgerblue'
+    "color": '#444'
   }
 ]
 
