df8b9069175872ca9fd55fbdcf47a2bddbc74bc6
Kevin Le <kevinphanle11@gmail.com>
2020-07-02 21:30:52 -0700

    responsive contact page

--- src/components/frontpage/team/team_members.js
+++ src/components/frontpage/team/team_members.js
@@ -19,9 +19,9 @@ let team = [
   },
   {
     "name": "Hank",
-    "description": "guard dog Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque a odio at fringilla. Cras dictum neque sed lacus rutrum pharetra. Etiam ut sapien fringilla, pellentesque purus sit amet, posuere nisl. Nulla nec risus ut diam mollis elementum a sed tortor.",
+    "description": "greeter Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque a odio at fringilla. Cras dictum neque sed lacus rutrum pharetra. Etiam ut sapien fringilla, pellentesque purus sit amet, posuere nisl. Nulla nec risus ut diam mollis elementum a sed tortor.",
     "photo": "https://balbin-upson-dev.s3-us-west-1.amazonaws.com/IMG_0651.JPG",
-    "title": "Guard Dog"
+    "title": "Greeter"
   }
 ];
 
