e8d8156ab6d4037be8c75525e43ededd84a14b9c
Kevin Le <kevinphanle11@gmail.com>
2020-08-26 14:21:57 -0700

    updated about me

--- src/components/aboutus/about.jsx
+++ src/components/aboutus/about.jsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import bert from '../../images/bert.JPG';
+import React from "react";
+import bert from "../../images/bert.JPG";
 
 function About(props) {
-
   return (
-    <div className="about">
-      <h2>A team of <span>big dreamers</span></h2>
-      <div className="header-img">
-        <img src="https://balbin-upson-dev.s3-us-west-1.amazonaws.com/team%401080.jpg" alt=""/>
-      </div>
+    <div className="about-container">
+      <div className="about">
+        <h2>
+          A team of <span>big dreamers</span>
+        </h2>
+        <div className="header-img">
+          <img
+            src="https://balbin-upson-dev.s3-us-west-1.amazonaws.com/team%401080.jpg"
+            alt="teamphoto"
+          />
+        </div>
 
-      <div className="aboutus">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit velit quas libero rem? Maxime, animi aliquam! Nihil culpa, dignissimos fugiat quam esse, tempora ut nemo excepturi dolor ducimus iste suscipit! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos ab numquam, deleniti ea omnis, eaque veritatis reiciendis repellendus maxime molestiae sint ut, eligendi cumque quod magni dicta consectetur laborum minima!
-          
-          <br />
-          
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. In animi reprehenderit laboriosam consectetur maiores repudiandae molestias veritatis tempore dolore aliquam quisquam quibusdam ad pariatur illum tempora provident, quas dignissimos quo?
-        </p>
+        <div className="aboutus">
+          <p>
+            <span>Balbin & Upson</span> started with the idea of helping friends and
+            neighbors with tax and finance questions.
+            <br />
+            <br /> Amy Balbin (who passed away in 2011) started as a one-person
+            tax wiz helping her husband’s Navy buddies complete their income
+            taxes while her husband, Camilo Balbin, and herself were stationed
+            in Corpus Christie, Texas. Balbin & Upson is now a brother and
+            sister team, consisting of Robert and Leah, that embody the spirit
+            of caring and helping others. <br />
+            <br /> As her legacy has passed on to Balbin & Upson, our vision has
+            stayed the course: <span>we are your local tax family building community
+            confidence through quality work and education.</span>
+          </p>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default About;
